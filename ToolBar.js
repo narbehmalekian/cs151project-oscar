54f567d0c3115398ca595acd745ba7ed1e2b1e50
Jeren <jeren.mckey@sjsu.edu>
2019-04-30 18:24:57 -0700

    Changed toolbar dynamics

--- ToolBar.js
+++ ToolBar.js
@@ -22,21 +22,11 @@ class ToolBar{
    {
 	    const div = document.getElementById('toolbar')
 		div.innerHTML = ''
-		const grabButton = document.createElement('button')
+		const grabButton = document.createElement('div')
 		grabButton.setAttribute('name', 'Grabber')
 		grabButton.setAttribute('class', 'toolbarButtons')
-		grabButton.addEventListener('click', event => {
-			let name = event.target.name
-			let found = false
-			for (let i = 0; i < this.prototypes.length; i++)
-			{
-				if (this.prototypes[i].constructor.name === name){
-					console.log("true")
-					this.currentTool = this.prototypes[i]
-					found = true
-				}
-			}
-			if (!found) this.currentTool = null
+		grabButton.addEventListener('mousedown', event => {
+			this.currentTool = null
 		})
 		div.appendChild(grabButton)
 		const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
@@ -55,11 +45,11 @@ class ToolBar{
 	    for (let i = 0; i < this.prototypes.length; i++)
 		{
 			const div = document.getElementById('toolbar')
-			const newButton = document.createElement('button')
+			const newButton = document.createElement('div')
 			newButton.setAttribute('name', this.prototypes[i].constructor.name)
 			newButton.setAttribute('class', 'toolbarButtons')
-			newButton.addEventListener('click', event => {
-				let name = event.target.name
+			newButton.addEventListener('mousedown', event => {
+				let name = event.target.attributes.name.textContent
 				let found = false
 				for (let i = 0; i < this.prototypes.length; i++)
 				{
@@ -80,14 +70,16 @@ class ToolBar{
 			svg.setAttribute('padding', '20px')
 			svg.setAttribute('width', iconWidth)
 			svg.setAttribute('height', iconHeight)
+			svg.setAttribute('name', this.prototypes[i].constructor.name)
 			svg.setAttribute('style', 'dispaly:none')
+			icon.setAttribute('name', this.prototypes[i].constructor.name)
 			svg.appendChild(icon)
 			newButton.appendChild(svg)
 		}
    }
 
    drawGrabber(x,y, svg){
-        const SIZE = 5
+        const SIZE = 8
         let rect = new Rectangle()
         rect.setRect(x - SIZE / 2, y - SIZE / 2, SIZE , SIZE)
         const sq = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
