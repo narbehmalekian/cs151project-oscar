1c9a435eb1e534d45af7d91c47370417aa8c992c
Alisha Mehndiratta <alisha8899@gmail.com>
2019-05-05 13:14:46 -0700

    Added jsdoc

--- SimpleEdge.js
+++ SimpleEdge.js
@@ -6,11 +6,20 @@
 */
 class SimpleEdge
 {
+    /** 
+    * Constructs a new SimpleEdge
+    * @param s the starting node
+    * @param e the ending node
+    */
     constructor(s, e){
         this.start = s
         this.end = e
         this.ENDSIZE = 10
     }
+
+    /**
+    * Draws the Simple Edge on the graphpanel context
+    */
 
     draw()
     {
@@ -23,7 +32,10 @@ class SimpleEdge
         l.setAttribute('style','stroke:rgb(0,0,0);stroke-width:2');
         panel.appendChild(l);   
     }
-
+    /**
+    * Returns a graphic of the Simple edge object
+    * @return an SVG element
+    */
     drawIcon(){
         let panel = document.getElementById('graphpanel');
         let l = document.createElementNS('http://www.w3.org/2000/svg', 'line');
@@ -37,14 +49,25 @@ class SimpleEdge
         return l;
     }
 
+    /**
+    * Returns the starting node for this edge
+    * @return a node object
+    */
     getStart(){ // start node
         return this.start;
     }
-
+    /**
+    * Returns the ending node for this edge
+    * @return a node object
+    */
     getEnd(){ // end node
         return this.end;
     }
-
+    /**
+    * Sets the starting and end nodes that are connected by this edge
+    * @param n1 the first node
+    * @param n2 the second node
+    */
     connect(n1, n2){
         this.start = n1
         this.end = n2
@@ -52,7 +75,11 @@ class SimpleEdge
         this.end.addEdge(this)
         this.draw()
     }
-
+    /**
+    * Returns true if a point is inside the Simple Edge object
+    * @param p a point object
+    * @return a boolean value
+    */
     contains(p){
       const MAX_DIST = 5
 	  let line = this.getConnectionPoints()
@@ -63,42 +90,68 @@ class SimpleEdge
 	  }
 	  return false
 	}
-
+    /**
+    * Gets the bounding rectangle for the Simple Edge shape
+    * @return a rectangle object
+    */
     getBounds(){
         let rect = new Rectangle(Math.min(this.startX(),this.endX()),Math.min(this.startY(),this.endY()),Math.abs(this.startX()-this.endX()),Math.abs(this.startY()-this.endY()));
         return rect;
     }
-
+    /** 
+    * Returns the starting point x-coordinate
+    * @return a number
+    */
     startX(){
         this.start.getConnectionPoint(this.end).getX();
     }
-
+    /** 
+    * Returns the starting point y-coordinate
+    * @return a number
+    */
     startY(){
         this.start.getConnectionPoint(this.end).getY();
     }
-
+    /** 
+    * Returns the ending point x-coordinate
+    * @return a number
+    */
     endX(){
         this.end.getConnectionPoint(this.start).getX();
     }
-
+    /** 
+    * Returns the ending point y-coordinate
+    * @return a number
+    */
     endY(){
         this.end.getConnectionPoint(this.start).getY();
     }
-
-
+    /**
+    * Creates a new Simple Edge object and returns that new object
+    * @return a SimpleEdge object
+    */
     clone(){
         let clone = new SimpleEdge()
         return clone
     }
-
+    /**
+    * Returns the width of the SimpleEdge graphic
+    * @return a number
+    */
     getIconWidth(){
         return 20;
     }
-
+    /**
+    * Returns the height of the SimpleEdge graphic
+    * @return a number
+    */
     getIconHeight(){
         return 20;
     }
-	
+    /**
+    * Returns the connections points for the edge based on the node connection points 
+    * @return a line from one node to another
+    */
    getConnectionPoints()
    {
       let startBounds = this.start.getBounds()
